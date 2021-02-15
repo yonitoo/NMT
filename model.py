84f07d8de46949d08786a22c9013bd65d52d6859
yonitoo <36246462+yonitoo@users.noreply.github.com>
2021-02-15 23:26:06 +0200

    fix minor error

--- model.py
+++ model.py
@@ -78,9 +78,9 @@ class NMTmodel(torch.nn.Module):
         tokens.append(self.word2ind_en["</S>"])
 
         sentence_tensor = torch.LongTensor(tokens).unsqueeze(1).to(device)
-        print(sentence_tensor.shape)
+
         with torch.no_grad():
-            enc_res, (h, c) = self.encoder(sentence_tensor)
+            enc_res, (h, c) = self.encoder(sentence_tensor, sentence_tensor.size())
         outputs = [self.word2ind_bg[startToken]]
 
         for _ in range(limit):
